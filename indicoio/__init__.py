08352578dfbf70ebe9fd07e693e2dfc25f1a701f
Madison May <madison@indico.io>
2015-07-22 02:48:31 -0400

    UPDATE: version number

--- indicoio/__init__.py
+++ indicoio/__init__.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-Version, version, __version__, VERSION = ('0.8.0',) * 4
+Version, version, __version__, VERSION = ('0.8.1',) * 4
 
 JSON_HEADERS = {
     'Content-type': 'application/json',

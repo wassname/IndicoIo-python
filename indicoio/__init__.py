ecc8c74c86bf513a3dc0e13f976f1637e95145c3
Chris Lee <sihrc.c.lee@gmail.com>
2015-07-30 18:19:57 -0400

    UPDATE: Version Tick

--- indicoio/__init__.py
+++ indicoio/__init__.py
@@ -1,7 +1,7 @@
 from functools import wraps, partial
 import warnings
 
-Version, version, __version__, VERSION = ('0.9.0',) * 4
+Version, version, __version__, VERSION = ('0.9.1',) * 4
 
 JSON_HEADERS = {
     'Content-type': 'application/json',
